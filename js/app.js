0d0ec355d8452244e945b27d13f443cf1e4b0201
Leonardo <leomicheloni@hotmail.com>
2014-11-29 20:39:00 -0300

    added basic route structure

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
-var app = angular.module("lang-puzzle", []);
+var app = angular.module("lang-puzzle", ["ngRoute"]);
+
+app.config(function($routeProvider){
+	$routeProvider.when("/", {
+		controller: "mainController",
+		templateUrl: "./templates/menu.html"
+	});
+
+	$routeProvider.when("/jugar", {
+		controller: "jugarController",
+		templateUrl: "./templates/jugar.html"
+	});
+
+	$routeProvider.when("/verIdiomas", {
+		controller: "verIdiomasController",
+		templateUrl: "./templates/verIdiomas.html"
+	});
+
+	$routeProvider.when("/agregarIdioma", {
+		controller: "agregarIdiomaController",
+		templateUrl: "./templates/agregarIdioma.html"
+	});
+	
+});
 
 app.controller("mainController", function($scope){
 	
 });
+
+app.controller("jugarController", function($scope){
+	
+});
+
+app.controller("agregarIdiomaController", function($scope){
+	
+});
+
+app.controller("verIdiomasController", function($scope){
+	
+});
+	
+	
+	
+	
+
+
