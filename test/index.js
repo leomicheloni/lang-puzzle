5b3bf376e966fa660ebd3aaf5b642ce1935265f4
Leonardo <=>
2014-12-28 17:24:10 -0300

    added new test to verify if we add two languages we can find what we want

--- test/index.js
+++ test/index.js
@@ -22,14 +22,29 @@ describe("puzzle service", function () {
 			var result = service.getLanguage(lang.name);			
 			expect(result.name).toBe("test");
 		});
-		
-		
+				
 		it("add language", function(){		
 			var name = "spanish";		
 			service.addLanguage(name);			
 			var lang = service.getLanguage(name);			
 			expect(lang).not.toBe(null);			
 			expect(lang.name).toBe(name);		
+		});
+
+		it("add two languages and find one", function(){		
+			var name = "spanish";		
+			service.addLanguage(name);
+
+			var name2 = "english";		
+			service.addLanguage(name2);
+			
+			var lang1 = service.getLanguage(name);			
+			expect(lang1).not.toBe(null);			
+			expect(lang1.name).toBe(name);		
+
+			var lang2 = service.getLanguage(name2);			
+			expect(lang2).not.toBe(null);			
+			expect(lang2.name).toBe(name2);		
 		});
 	});
 
