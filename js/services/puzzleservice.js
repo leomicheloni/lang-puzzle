e3f9fd7ee7c5105c65affab919cd99bfb140560a
Leonardo <=>
2014-12-28 17:10:28 -0300

    add language implemented

--- js/services/puzzleservice.js
+++ js/services/puzzleservice.js
@@ -1,5 +1,40 @@
 //main service, controls all game logic
 app.factory("puzzleService", function () {
+
 	return {
+
+		languages : [],
+
+		getLangMaxId : function () {
+			if(this.languages.length == 0) return 0;
+			
+			this.languages.sort(function (a, b) {
+				a.id > b.id;
+			});
+
+			return this.languages[this.languages.length - 1].id;
+		},
+
+		newLang : function (name) {
+			var maxId = this.getLangMaxId();
+			return {
+				id : maxId + 1,
+				name : name,
+				words : []
+			}
+		},
+
+		addLanguage : function (name) {
+			var newLang = this.newLang(name);
+			this.languages.push(newLang);
+		},
+
+		getLanguage : function (name) {
+			return this.languages.filter(function (item) {
+				item.name == name;
+			})[0];
+
+		}
+
 	};
 });
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-describe("test1", function () {
+describe("puzzle service", function () {
 	var service;
 
 	beforeEach(function () {
@@ -9,5 +9,18 @@ describe("test1", function () {
 	beforeEach(inject(function ($rootScope, $controller, $injector) {
 		service = $injector.get("puzzleService");
 	}));
+
+	describe("language", function(){
+		it("add language", function(){
+		
+			service.addLanguage("spanish");
+			
+			var lang = service.getLanguage("spanish");
+			
+			expect(lang).not.toBe(null);
+		
+		});
+	});
+
 
 });
